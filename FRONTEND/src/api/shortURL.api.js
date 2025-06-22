53751693df423ce0816595693f145ce7e18c4ce6
Ayush7403 <ayushsoni7403@gmail.com>
2025-06-23 00:45:41 +0530

    urfFEapi

--- FRONTEND/src/api/shortURL.api.js
+++ FRONTEND/src/api/shortURL.api.js
@@ -3,7 +3,7 @@ import axiosInstance from "../utils/axiosInstance";
 export const createShortUrl = async (url, slug) => {
   const { data } = await axiosInstance.post("/api/create", {
     full_url: url,
-    ...(slug && { custom_slug: slug }), // add only if slug exists
+    ...(slug && { custom_slug: slug }),
   });
   return data.shortUrl;
 };
